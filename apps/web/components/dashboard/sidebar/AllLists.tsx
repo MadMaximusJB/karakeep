hoarder-app/hoarder

Feat: add locked lists feature. Enables users to lock lists, requiring a unique, encrypted password to access, excludes locked list content from homepage.

apps/web/components/dashboard/sidebar/AllLists.tsx
@@ -9,7 +9,7 @@ import { Button } from "@/components/ui/button";
 import { CollapsibleTriggerTriangle } from "@/components/ui/collapsible";
 import { useTranslation } from "@/lib/i18n/client";
 import { cn } from "@/lib/utils";
-import { CirclePlus, MoreHorizontal } from "lucide-react";
+import { CirclePlus, Lock, MoreHorizontal } from "lucide-react";
 
 import type { ZBookmarkList } from "@karakeep/shared/types/lists";
 import { ZBookmarkListTreeNode } from "@karakeep/shared/utils/listUtils";
@@ -70,8 +70,11 @@ export default function AllLists({
               )
             }
             logo={
-              <span className="flex">
+              <span className="flex items-center gap-1">
                 <span className="text-lg"> {node.item.icon}</span>
+                {node.item.locked && (
+                  <Lock className="size-3 text-muted-foreground" />
+                )}
               </span>
             }
             name={node.item.name}

packages/trpc/routers/lists.ts
@@ -26,12 +26,22 @@ export const ensureListOwnership = experimental_trpcMiddleware<{
   });
 });
 
+export const ensureListAccess = experimental_trpcMiddleware<{
+  ctx: AuthedContext & { list: List };
+  input: { password?: string };
+}>().create(async (opts) => {
+  await opts.ctx.list.ensureCanAccessLocked(opts.input.password);
+  return opts.next({
+    ctx: opts.ctx,
+  });
+});
+
 export const listsAppRouter = router({
   create: authedProcedure
     .input(zNewBookmarkListSchema)
     .output(zBookmarkListSchema)
     .mutation(async ({ input, ctx }) => {
-      return await List.create(ctx, input).then((l) => l.list);
+      return await List.create(ctx, input).then((l) => l.publicList);
     }),
   edit: authedProcedure
     .input(zEditBookmarkListSchemaWithValidation)
@@ -39,7 +49,7 @@ export const listsAppRouter = router({
     .use(ensureListOwnership)
     .mutation(async ({ input, ctx }) => {
       await ctx.list.update(input);
-      return ctx.list.list;
+      return ctx.list.publicList;
     }),
   merge: authedProcedure
     .input(zMergeListSchema)
@@ -68,9 +78,11 @@ export const listsAppRouter = router({
       z.object({
         listId: z.string(),
         bookmarkId: z.string(),
+        password: z.string().optional(),
       }),
     )
     .use(ensureListOwnership)
+    .use(ensureListAccess)
     .use(ensureBookmarkOwnership)
     .mutation(async ({ input, ctx }) => {
       await ctx.list.addBookmark(input.bookmarkId);
@@ -80,9 +92,11 @@ export const listsAppRouter = router({
       z.object({
         listId: z.string(),
         bookmarkId: z.string(),
+        password: z.string().optional(),
       }),
     )
     .use(ensureListOwnership)
+    .use(ensureListAccess)
     .use(ensureBookmarkOwnership)
     .mutation(async ({ input, ctx }) => {
       await ctx.list.removeBookmark(input.bookmarkId);
@@ -96,7 +110,7 @@ export const listsAppRouter = router({
     .output(zBookmarkListSchema)
     .use(ensureListOwnership)
     .query(({ ctx }) => {
-      return ctx.list.list;
+      return ctx.list.publicList;
     }),
   list: authedProcedure
     .output(
@@ -105,8 +119,9 @@ export const listsAppRouter = router({
       }),
     )
     .query(async ({ ctx }) => {
-      const results = await List.getAll(ctx);
-      return { lists: results.map((l) => l.list) };
+      // Include locked lists for the owner - they should see their own locked lists in UI
+      const results = await List.getAll(ctx, true);
+      return { lists: results.map((l) => l.publicList) };
     }),
   getListsOfBookmark: authedProcedure
     .input(z.object({ bookmarkId: z.string() }))
@@ -118,7 +133,7 @@ export const listsAppRouter = router({
     .use(ensureBookmarkOwnership)
     .query(async ({ input, ctx }) => {
       const lists = await List.forBookmark(ctx, input.bookmarkId);
-      return { lists: lists.map((l) => l.list) };
+      return { lists: lists.map((l) => l.publicList) };
     }),
   stats: authedProcedure
     .output(
@@ -129,7 +144,9 @@ export const listsAppRouter = router({
     .query(async ({ ctx }) => {
       const lists = await List.getAll(ctx);
       const sizes = await Promise.all(lists.map((l) => l.getSize()));
-      return { stats: new Map(lists.map((l, i) => [l.list.id, sizes[i]])) };
+      return {
+        stats: new Map(lists.map((l, i) => [l.publicList.id, sizes[i]])),
+      };
     }),
 
   // Rss endpoints
@@ -173,5 +190,50 @@ export const listsAppRouter = router({
     .query(async ({ input, ctx }) => {
       const list = await List.fromId(ctx, input.listId);
       return { token: await list.getRssToken() };
+    }),
+
+  // Locked list endpoints
+  verifyListPassword: authedProcedure
+    .input(
+      z.object({
+        listId: z.string(),
+        password: z.string(),
+      }),
+    )
+    .output(
+      z.object({
+        valid: z.boolean(),
+      }),
+    )
+    .use(ensureListOwnership)
+    .mutation(async ({ input, ctx }) => {
+      const valid = await ctx.list.verifyPassword(input.password);
+      return { valid };
+    }),
+
+  getWithPassword: authedProcedure
+    .input(
+      z.object({
+        listId: z.string(),
+        password: z.string().optional(),
+      }),
+    )
+    .output(zBookmarkListSchema)
+    .use(ensureListOwnership)
+    .query(async ({ input, ctx }) => {
+      await ctx.list.ensureCanAccessLocked(input.password);
+      return ctx.list.publicList;
+    }),
+
+  getLockedLists: authedProcedure
+    .output(
+      z.object({
+        lists: z.array(zBookmarkListSchema),
+      }),
+    )
+    .query(async ({ ctx }) => {
+      const allLists = await List.getAll(ctx, true);
+      const lockedLists = allLists.filter((l) => l.publicList.locked);
+      return { lists: lockedLists.map((l) => l.publicList) };
     }),
 });

MadMaximusJB/karakeep

apps/web/components/dashboard/sidebar/AllLists.tsx
@@ -14,7 +14,7 @@ import { toast } from "@/components/ui/sonner";
 import { BOOKMARK_DRAG_MIME } from "@/lib/bookmark-drag";
 import { useTranslation } from "@/lib/i18n/client";
 import { cn } from "@/lib/utils";
-import { MoreHorizontal, Plus } from "lucide-react";
+import { Lock, MoreHorizontal, Plus } from "lucide-react";
 
 import type { ZBookmarkList } from "@karakeep/shared/types/lists";
 import {
@@ -119,8 +119,11 @@ function DroppableListSidebarItem({
         )
       }
       logo={
-        <span className="flex">
+        <span className="flex items-center gap-1">
           <span className="text-lg"> {node.item.icon}</span>
+          {node.item.locked && (
+            <Lock className="size-3 text-muted-foreground" />
+          )}
         </span>
       }
       name={node.item.name}

packages/trpc/routers/lists.ts
@@ -12,6 +12,7 @@ import { addLogFields, logEvent } from "@karakeep/shared-server";
 
 import type { AuthedContext } from "../index";
 import {
+  authedProcedure,
   createEventLogMiddleware,
   createRateLimitMiddleware,
   createScopedAuthedProcedure,
@@ -42,6 +43,16 @@ export const ensureListAtLeastEditor = experimental_trpcMiddleware<{
   input: { listId: string };
 }>().create(async (opts) => {
   opts.ctx.list.ensureCanEdit();
+  return opts.next({
+    ctx: opts.ctx,
+  });
+});
+
+export const ensureListAccess = experimental_trpcMiddleware<{
+  ctx: AuthedContext & { list: List };
+  input: { password?: string };
+}>().create(async (opts) => {
+  await opts.ctx.list.ensureCanAccessLocked(opts.input.password);
   return opts.next({
     ctx: opts.ctx,
   });
@@ -139,6 +150,7 @@ export const listsAppRouter = router({
     )
     .use(ensureListAtLeastViewer)
     .use(ensureListAtLeastEditor)
+    .use(ensureListAccess)
     .use(ensureBookmarkOwnership)
     .mutation(async ({ input, ctx }) => {
       await ctx.list.addBookmark(input.bookmarkId);
@@ -152,6 +164,7 @@ export const listsAppRouter = router({
     )
     .use(ensureListAtLeastViewer)
     .use(ensureListAtLeastEditor)
+    .use(ensureListAccess)
     .mutation(async ({ input, ctx }) => {
       await ctx.list.removeBookmark(input.bookmarkId);
     }),
@@ -411,5 +424,49 @@ export const listsAppRouter = router({
     .use(ensureListAtLeastViewer)
     .mutation(async ({ ctx }) => {
       await ctx.list.leaveList();
+    }),
+
+  verifyListPassword: authedProcedure
+    .input(
+      z.object({
+        listId: z.string(),
+        password: z.string(),
+      }),
+    )
+    .output(
+      z.object({
+        valid: z.boolean(),
+      }),
+    )
+    .use(ensureListAtLeastViewer)
+    .mutation(async ({ input, ctx }) => {
+      const valid = await ctx.list.verifyPassword(input.password);
+      return { valid };
+    }),
+
+  getWithPassword: authedProcedure
+    .input(
+      z.object({
+        listId: z.string(),
+        password: z.string().optional(),
+      }),
+    )
+    .output(zBookmarkListSchema)
+    .use(ensureListAtLeastViewer)
+    .query(async ({ input, ctx }) => {
+      await ctx.list.ensureCanAccessLocked(input.password);
+      return ctx.list.asZBookmarkList();
+    }),
+
+  getLockedLists: authedProcedure
+    .output(
+      z.object({
+        lists: z.array(zBookmarkListSchema),
+      }),
+    )
+    .query(async ({ ctx }) => {
+      const allLists = await List.getAll(ctx);
+      const lockedLists = allLists.filter((l) => l.asZBookmarkList().locked);
+      return { lists: lockedLists.map((l) => l.asZBookmarkList()) };
     }),
 });